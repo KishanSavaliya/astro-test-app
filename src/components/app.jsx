c8f184fd009a1d578b1123b118053294c3f1d5c1
Kishan Savaliya <kishansavaliyakb@gmail.com>
2024-09-07 18:24:08 +0530

    sdasda

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -10,10 +10,12 @@ const App = () => {
       <nav>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <Link to="/test">Test</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </Router>
   );
